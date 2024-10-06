1a152d7569e640f109def4c59ef1b491db9901c3
Mohan200621 <24104116@nec.edu.in>
2024-10-06 13:52:14 +0530

    Add files via upload
    created automatic on/off fan with if else statement
    with one month PST class

---
+++ automatic fan on off.c
@@ -0,0 +1,99 @@
+//Automatic fan on/off system
+#include <stdio.h>
+int main()
+{
+int fan1,fan2,fan3,fan4;
+printf("Enter fan1:");
+scanf("%d",&fan1);
+//fan1 start
+{
+if(fan1==1)
+    printf("\nFan1 is on");
+else if(fan1==0)
+
+    if(fan1==1)
+    {
+        printf("\nFan1 is off");
+
+    }
+    else
+    {
+        printf("Fan1 already in off\n");
+    }
+
+}
+//fan2 start
+printf("Enter fan2:\n");
+scanf("%d",&fan2);
+{
+
+
+if(fan2==1)
+
+{
+    printf("Fan2 is on\n");
+}
+else if(fan2==0)
+
+    if(fan2==1)
+    {
+        printf("Fan2 is off\n");
+
+    }
+    else
+    {
+        printf("Fan2 already in off\n");
+    }
+}
+//fan3 start
+printf("Enter fan3:\n");
+scanf("%d",&fan3);
+{
+
+
+if(fan3==1)
+
+{
+    printf("Fan3 is on\n");
+}
+else if(fan3==0)
+
+    if(fan3==1)
+    {
+        printf("Fan3 is off\n");
+
+    }
+    else
+    {
+        printf("Fan3 already in off\n");
+    }
+}
+
+//fan4 start
+printf("Enter fan4:\n");
+scanf("%d",&fan4);
+{
+
+
+if(fan4==1)
+
+{
+    printf("Fan4 is on\n");
+}
+else if(fan4==0)
+
+    if(fan4==1)
+    {
+        printf("Fan4 is off\n");
+
+    }
+    else
+    {
+        printf("Fan4 already in off\n");
+    }
+}
+return 0;
+}
+
+
+
